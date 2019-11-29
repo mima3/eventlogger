f60b403c1e8f05d68a41330e2ee619cd85ab00a3
mima3 <mima@js6.so-net.ne.jp>
2019-11-30 05:38:40 +0900

    eslint対応

--- js/eventlogger.js
+++ js/eventlogger.js
@@ -1,5 +1,5 @@
 // 以下参考にしながら実装
-//https://github.com/SeleniumHQ/selenium-ide/blob/61eddf725e1a4422e9379c5a8e3b8d5a765bda78/packages/side-recorder/src/content/record-handlers.js
+// https://github.com/SeleniumHQ/selenium-ide/blob/61eddf725e1a4422e9379c5a8e3b8d5a765bda78/packages/side-recorder/src/content/record-handlers.js
 // args.window : 監視対象のwindow
 // args.writeLogCallback : ログ書き込み時のコールバック
 // args.secretElements : inputでvalueを隠す対象の要素の配列
@@ -17,7 +17,7 @@ const EventLogger = function(args) {
       this.writeLogCallback(this.events);
     }
   }.bind(this));
-  
+
   this.secretElements = args.secretElements;
   if (!this.secretElements) {
     this.secretElements = [];
@@ -27,15 +27,15 @@ const EventLogger = function(args) {
   this.lastEventTime = 0;
   this.lastEventName = '';
   this.lastLocator = '';
-}
+};
 EventLogger.prototype.writeLog = function() {
   this.writeLogCallback(this.events);
   this.events = [];
 };
 
 EventLogger.prototype.record = function(eventName, target, params, waitTime) {
-  let time = (new Date()).getTime();
-  let locator = this.buildLocator(target);
+  const time = (new Date()).getTime();
+  const locator = this.buildLocator(target);
   if (this.lastEventName === eventName &&
       JSON.stringify(this.lastLocator) === JSON.stringify(locator) &&
      (time - this.lastEventTime < waitTime)) {
@@ -45,86 +45,85 @@ EventLogger.prototype.record = function(eventName, target, params, waitTime) {
     return;
   }
   this.events.push({
-    time : time,
-    eventName : eventName,
-    locator : locator,
-    params : params
+    time: time,
+    eventName: eventName,
+    locator: locator,
+    params: params,
   });
   this.lastEventName = eventName;
   this.lastLocator = locator;
   this.lastEventTime = time;
-}
+};
 
 EventLogger.prototype.buildLocator = function(el) {
   // IDで解決できるならID
-  if (el.id && (typeof (el.id) == "string" || el.id instanceof String)) {
-    return {id:el.id };
+  if (el.id && (typeof (el.id) == 'string' || el.id instanceof String)) {
+    return {id: el.id};
   }
   // nameで解決できるならname
-  if (el.name && (typeof (el.name) == "string" || el.name instanceof String)) {
-    let nameResult = this.window.document.getElementsByName(el.name);
-    return {name:el.name, nameIndex: this.getNodeNbrSameName(el)};
+  if (el.name && (typeof (el.name) == 'string' || el.name instanceof String)) {
+    return {name: el.name, nameIndex: this.getNodeNbrSameName(el)};
   }
   // positionで解決する
   position = this.relativePosition(el);
   if (position) {
-    return { position : position};
+    return {position: position};
   }
-  
+
   // あきらめる
   return {};
-}
+};
 
 EventLogger.prototype.getNodeNbrSameName = function(current) {
-  let childNodes = current.parentNode.childNodes;
+  const childNodes = current.parentNode.childNodes;
   let total = 0;
   let index = -1;
   for (let i = 0; i < childNodes.length; i++) {
-    let child = childNodes[i];
+    const child = childNodes[i];
     if (child.nodeName == current.nodeName && child.name == current.name) {
       if (child == current) {
         index = total;
       }
-      total++
+      total++;
     }
   }
   return index;
-}
+};
 
 
 EventLogger.prototype.getNodeNbr = function(current) {
-  let childNodes = current.parentNode.childNodes;
+  const childNodes = current.parentNode.childNodes;
   let total = 0;
   let index = -1;
   for (let i = 0; i < childNodes.length; i++) {
-    let child = childNodes[i];
+    const child = childNodes[i];
     if (child.nodeName == current.nodeName) {
       if (child == current) {
         index = total;
       }
-      total++
+      total++;
     }
   }
   return index;
-}
+};
 
 EventLogger.prototype.relativePathFromParent = function(current) {
-  let index = this.getNodeNbr(current);
+  const index = this.getNodeNbr(current);
   let currentPath = '/' + current.nodeName.toLowerCase();
   currentPath += '[' + (index) + ']';
   return currentPath;
-}
+};
 
 EventLogger.prototype.relativePosition = function(el) {
   let path = '';
   let current = el;
-  while(current.parentNode != null) {
-    let currentPath = this.relativePathFromParent(current);
+  while (current.parentNode != null) {
+    const currentPath = this.relativePathFromParent(current);
     path = currentPath + path;
     current = current.parentNode;
   }
   return path;
-}
+};
 
 
 EventLogger.prototype.clickHandler = function(event) {
@@ -137,83 +136,81 @@ EventLogger.prototype.clickHandler = function(event) {
   this.preventClickTwice = true;
 
   this.record(
-    'click',
-    event.target,
-    {
-      offsetX : event.offsetX,
-      offsetY : event.offsetY
-    },
-    0
+      'click',
+      event.target,
+      {
+        offsetX: event.offsetX,
+        offsetY: event.offsetY,
+      },
+      0,
   );
-  let self = this;
+  const self = this;
   setTimeout(function() {
     self.preventClickTwice = false;
   }, 30);
-}
+};
 
 EventLogger.prototype.scrollHandler = function(event) {
   this.record(
-    'scroll',
-    event.target,
-    {
-      x : window.pageXOffset ,
-      y : window.pageYOffset 
-    },
-    1000
+      'scroll',
+      event.target,
+      {
+        x: window.pageXOffset,
+        y: window.pageYOffset,
+      },
+      1000,
   );
-}
+};
 
 EventLogger.prototype.changeHandler = function(event) {
   if (!event.target.tagName) {
     return;
   }
-  let tagName = event.target.tagName.toLowerCase();
-  let typeName = event.target.type.toLowerCase();
+  const tagName = event.target.tagName.toLowerCase();
+  const typeName = event.target.type.toLowerCase();
   if ('input' === tagName) {
     if (typeName === 'checkbox' || typeName === 'radio') {
       return this.changeCheckHandler(event);
     } else {
       return this.changeInputHandler(event);
     }
-  }
-  else if ('textarea' === tagName) {
+  } else if ('textarea' === tagName) {
     return this.changeInputHandler(event);
-  }
-  else if ('select' === tagName) {
+  } else if ('select' === tagName) {
     return this.changeSelectHandler(event);
   }
   return;
-}
+};
 EventLogger.prototype.changeInputHandler = function(event) {
   let value = event.target.value;
-  let typeName = event.target.type.toLowerCase();
-  if (event.target.type === 'password' ||
+  const typeName = event.target.type.toLowerCase();
+  if (typeName === 'password' ||
       this.secretElements.indexOf(event.target) >= 0) {
     value = '*****';
   }
   this.record(
-    'input',
-    event.target,
-    {
-      value : value
-    },
-    0
+      'input',
+      event.target,
+      {
+        value: value,
+      },
+      0,
   );
-}
+};
 EventLogger.prototype.changeCheckHandler = function(event) {
   this.record(
-    'check',
-    event.target,
-    {
-      checked : event.target.checked
-    },
-    0
+      'check',
+      event.target,
+      {
+        checked: event.target.checked,
+      },
+      0,
   );
-}
+};
 
 EventLogger.prototype.changeSelectHandler = function(event) {
-  let selectIndex = [];
-  let options = event.target.options;
+  const selectIndex = [];
+  const options = event.target.options;
   if (event.target.multiple) {
     for (let i = 0; i < options.length; i++) {
       if (options[i].selected) {
@@ -224,23 +221,23 @@ EventLogger.prototype.changeSelectHandler = function(event) {
     selectIndex.push(options.selectedIndex);
   }
   this.record(
-    'select',
-    event.target,
-    {
-      selectIndex : selectIndex
-    },
-    0
+      'select',
+      event.target,
+      {
+        selectIndex: selectIndex,
+      },
+      0,
   );
-}
+};
 EventLogger.prototype.messageHandler = function(event) {
   this.record(
-    'message',
-    event.target,
-    {
-      type : event.data.recordedType,
-      message : event.data.recordedMessage,
-      result : event.data.recordedResult
-    },
-    0
+      'message',
+      event.target,
+      {
+        type: event.data.recordedType,
+        message: event.data.recordedMessage,
+        result: event.data.recordedResult,
+      },
+      0,
   );
-}
+};
